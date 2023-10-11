98e90bacd91023ff83244f77f2a78040f33ecc25
Hung Hoang <hunghq.work@gmail.com>
2023-10-11 14:45:31 +0700

    Library

--- SnakeGame/GameManager.cpp
+++ SnakeGame/GameManager.cpp
@@ -1,4 +1,5 @@
 ﻿#include "GameManager.h"
+#include <imgui.h>
 
 GameManager::GameManager()
 {
--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,6 +1,9 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <thread>
 
+#include "imgui.h"
+#include "imgui-SFML.h"
+
 #include "LevelBorder.h"
 #include "Snake.h"
 #include "main.h"
@@ -14,6 +17,7 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(GameConfig::WINDOW_WIDTH, GameConfig::WINDOW_HEIGHT), "Snake Game");
     window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
     
     GameManager* gameManager = new GameManager();
     
@@ -39,13 +43,24 @@ int main()
         sf::Time deltaTimeTimer = clock.restart(); // lấy thời gian đã trôi qua và khởi động lại
         float deltaTime = deltaTimeTimer.asSeconds();
 
+        ImGui::SFML::Update(window, deltaTimeTimer);
+
+        //ImGui::ShowDemoWindow();
+        ImGui::Begin("New Window");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
         gameManager->moveControl(deltaTime);
         gameManager->Update(deltaTime);
+
 
         // render lên màn hình tất cả những gì đã tính toán xong
         window.clear();
         gameManager->drawFruit(window);
         gameManager->drawSnake(window);
+
+        ImGui::SFML::Render(window);
+
         window.display();
     }
 
